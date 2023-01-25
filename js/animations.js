c9a42ea89d00a8945c14a6802b13d01cd9d734f6
Yana Karpliuk <yanakarpliuk99@gmail.com>
2023-01-25 12:46:41 +0200

    added hover effects and animation to hero

--- js/animations.js
+++ js/animations.js
@@ -1,19 +1,18 @@
-window.addEventListener('load', function () {
-  let animating = false;
-  const airplane = document.querySelector('.js-airplane');
+window.addEventListener("load", function () {
+  const plane2 = document.querySelector(".js-plane_2");
+  const plane1 = document.querySelector(".js-plane_1");
 
-  function startAnimation() {
-    if (animating) return;
+  function startAnimation(plane) {
+    plane.classList.add("hero__images-plane_animated");
+  }
 
-    animating = true;
-    airplane.classList.add('hero__images-plane_animated');
+  function endAnimation(plane) {
+    plane.classList.remove("hero__images-plane_animated");
   }
-  
-  function endAnimation() {
-    animating = false;
-    airplane.classList.remove('hero__images-plane_animated');
-  }
-  
-  airplane.addEventListener('mouseover', startAnimation);
-  airplane.addEventListener('animationend', endAnimation);
-})
+
+  plane1.addEventListener("mouseover", () => startAnimation(plane1));
+  plane1.addEventListener("animationend", () => endAnimation(plane1));
+
+  plane2.addEventListener("mouseover", () => startAnimation(plane2));
+  plane2.addEventListener("animationend", () => endAnimation(plane2));
+});
